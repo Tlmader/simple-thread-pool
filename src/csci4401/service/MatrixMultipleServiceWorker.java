f2cc4f972d6c3cfd902a899086a42404236dbdfc
Ted Mader <ted.mader@ge.com>
2016-11-03 13:28:20 -0500

    Add csci4401.service package and Java files

---
+++ src/csci4401/service/MatrixMultipleServiceWorker.java
@@ -0,0 +1,10 @@
+package csci4401.service;
+
+/**
+ * Description.
+ *
+ * @author tlmader.dev@gmail.com
+ * @since 2016-11-03
+ */
+public class MatrixMultipleServiceWorker {
+}
---
+++ src/csci4401/service/TestService.java
@@ -0,0 +1,10 @@
+package csci4401.service;
+
+/**
+ * Demonstrates that all implementations work correctly.
+ *
+ * @author tlmader.dev@gmail.com
+ * @since 2016-11-03
+ */
+public class TestService {
+}
