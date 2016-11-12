e0810e096ca4702567f453d6987a9d044bf5779d
Ted Mader <ted.mader@ge.com>
2016-11-11 19:18:38 -0600

    Add provided classes for tests

--- test/csci4401/service/TestService.java
+++ test/csci4401/service/TestService.java
@@ -1,10 +1,42 @@
 package csci4401.service;
 
+import csci4401.service.impl.MatrixMultiplyParameters;
+import csci4401.service.impl.MatrixMultiplyServicePool;
+
 /**
- * Demonstrates that all implementations work correctly.
- *
- * @author tlmader.dev@gmail.com
- * @since 2016-11-03
+ * Example test code; lacks timing. Use as needed.
  */
 public class TestService {
+
+    public static void main(String[] argv) throws Exception {
+        sequentialTest();
+        parallelTest();
+    }
+    /**
+     * Launches the workers one after the other.
+     */
+    static private void sequentialTest() throws Exception {
+        MatrixMultiplyServicePool servicePool = new MatrixMultiplyServicePool(5, 10);
+
+        int max = 8;
+        for(int i=0; i<max; i++) {
+            servicePool.addRequest(new MatrixMultiplyParameters(200, 100));
+            System.out.println(servicePool.getResponse());
+        }
+    }
+
+    /**
+     * Launches the workers in parallel.
+     */
+    static private void parallelTest() throws Exception {
+        MatrixMultiplyServicePool servicePool = new MatrixMultiplyServicePool(5, 10);
+
+        int max = 8;
+        for(int i=0; i<max; i++) {
+            servicePool.addRequest(new MatrixMultiplyParameters(200, 100));
+        }
+        for(int i=0; i<max; i++) {
+            System.out.println(servicePool.getResponse());
+        }
+    }
 }
